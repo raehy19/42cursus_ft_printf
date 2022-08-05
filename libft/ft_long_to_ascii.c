f242f5178f85678710d397bbdb148d695f28ba01
Raehyeon Jeong <rjeong@student.42seoul.kr>
2022-08-05 16:34:26 +0900

    feat : add libft functions

---
+++ libft/ft_long_to_ascii.c
@@ -0,0 +1,72 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_ltoa.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjeong <rjeong@student.42seoul.kr>         +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/07/11 15:23:02 by rjeong            #+#    #+#             */
+/*   Updated: 2022/07/11 16:18:10 by rjeong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stdlib.h"
+
+char	*ft_strrev(char *str, long long len)
+{
+	long long	i;
+	char		temp;
+
+	i = 0;
+	while (i < len - 1 - i)
+	{
+		temp = *(str + i);
+		*(str + i) = *(str + len - 1 - i);
+		*(str + len - 1 - i) = temp;
+		++i;
+	}
+	return (str);
+}
+
+long long	ft_cal_digit(long long n)
+{
+	long long	digit;
+
+	digit = 1;
+	while (n > 9)
+	{
+		n /= 10;
+		++digit;
+	}
+	return (digit);
+}
+
+long long	ft_cal_ltoa(char *dst, long long i, long long num)
+{
+	*(dst + i) = '0' + (num % 10);
+	if (num < 10)
+		return (i);
+	else
+		return (ft_cal_ltoa(dst, i + 1, num / 10));
+}
+
+char	*ft_long_to_ascii(long long n)
+{
+	char		*dst;
+	long long	size;
+
+	size = 0;
+	if (n < 0)
+	{
+		n *= -1;
+		++size;
+	}
+	size += ft_cal_digit(n);
+	dst = (char *) malloc (sizeof(char) * (size + 1));
+	if (!dst)
+		return (NULL);
+	if (ft_cal_ltoa(dst, 0, n) + 1 < size)
+		*(dst + size - 1) = '-';
+	*(dst + size) = '\0';
+	return (ft_strrev(dst, size));
+}
---
+++ libft/libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rjeong <rjeong@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/05 16:13:45 by rjeong            #+#    #+#             */
+/*   Updated: 2022/08/05 16:13:46 by rjeong           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+#define LIBFT_H
+
+#include "stdarg.h"
+#include "stdlib.h"
+#include "unistd.h"
+
+int		ft_strlen(const char *s);
+char	*ft_long_to_ascii(long long n);
+char	*ft_hex_to_ascii_upper(long long n);
+char	*ft_hex_to_ascii_lower(long long n);
+
+#endif
